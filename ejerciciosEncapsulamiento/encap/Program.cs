f9ca46e49ba5d812c9574567fc70cba0bda6e8d5
lucasBonggio <lucasbonggio1903@gmail.com>
2026-06-14 21:12:22 -0300

    Subida de ejercicios sobre encapsulamiento.

---
+++ ejerciciosEncapsulamiento/encap/Program.cs
@@ -0,0 +1,345 @@
+﻿using System.Collections;
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.ConstrainedExecution;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace encap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CuentaBancaria cuenta = new CuentaBancaria();
+
+
+            cuenta.depositarSaldo(0);
+            cuenta.depositarSaldo(1500);
+
+            cuenta.retirarDinero(1200);
+
+            Usuario usuario = new Usuario("lukario", "12312345");
+
+
+            Servidor servidor = new Servidor("192.9.0.0.1");
+
+            Console.WriteLine("Está conectado? " + servidor.getEstaConectado());
+
+            servidor.HacerPing();
+
+            Console.WriteLine("Está conectado? " + servidor.getEstaConectado());
+
+
+            Vaca vaca = new Vaca("Lola");
+            Gato gato = new Gato("Braulio");
+            Perro perro = new Perro("Elizamon");
+
+            Animal[] animales = new Animal[3];
+
+            animales[0] = vaca;
+            animales[1] = gato;
+            animales[2] = perro;
+
+            foreach (Animal animal in animales)
+            {
+                animal.HacerRuido();   
+            }
+
+
+            Router router1 = new Router("192.2.3.24");
+            Switch switch1 = new Switch("9.12.18.8");
+            Router router2 = new Router("192.5.3.00");
+            Switch switch2 = new Switch("7.77.77.77");
+
+            DispositivoRed[] dispositivos = new DispositivoRed[4];
+
+            dispositivos[0] = router1;
+            dispositivos[1] = switch1;
+            dispositivos[3] = switch2;
+            dispositivos[2] = router2;
+
+            foreach(DispositivoRed dispositivo in dispositivos)
+            {
+                dispositivo.EjecutarDiagnostico();
+            }
+
+        }
+    }
+
+    //Ejercicio 1: El Cajero Automático
+    //Crea una clase CuentaBancaria que tenga un atributo private decimal saldo.Crea
+    //un método público para depositar y un setter para retirar dinero.
+    //Regla de negocio: El setter o método de retiro debe verificar que el monto a retirar
+    //no sea mayor al saldo actual ni negativo.Si lo es, debe mostrar un mensaje de
+    //error por consola en lugar de restar el dinero.
+
+    public class CuentaBancaria
+    {
+        private decimal saldo;
+
+        public void depositarSaldo(decimal deposito)
+        {
+            if(deposito <= 0)
+            {
+                Console.WriteLine("El deposito tiene que ser superior a 0.");
+            }
+            else
+            {
+                this.saldo += deposito;
+                Console.WriteLine("Su saldo ha sido depositado.");
+                Console.WriteLine($"Saldo actual: ${this.saldo}");
+            }
+        }
+
+        public void retirarDinero(decimal retiro)
+        {
+            if (retiro  > 0 & this.saldo >= retiro)
+            {
+                this.saldo -= retiro;
+                Console.WriteLine("El retiro se completo exitosamente.");
+                Console.WriteLine($"Saldo actual: ${this.saldo}");
+            }else if(retiro > this.saldo)
+            {
+                Console.WriteLine("Fondos insuficientes.");
+            }
+        }
+    }
+
+    //Ejercicio 2: El Videojuego
+    //Programa una clase Personaje con los atributos private int vida y private string
+    //nombre.
+    //Regla de negocio: A través del setter, verifica que la vida del personaje nunca
+    //pueda bajar de 0 (si recibe mucho daño, queda en 0) ni subir de 100 (si toma una
+    //poción, el máximo es 100). Imprimí el estado del personaje en el Main.
+
+    public class Personaje
+    {
+        private int vida;
+        private string nombre;
+
+        public void setVida(int vida)
+        {
+            if(vida < 0)
+            {
+                this.vida -= vida;
+                if(this.vida < 0)
+                {
+                    this.vida = 0;
+                }
+            }else if(vida > 0)
+            {
+                this.vida += vida;
+                if(this.vida > 100)
+                {
+                    this.vida = 100;
+                }
+            }
+        }
+
+        public void getVida()
+        {
+
+        }
+    }
+
+    //Ejercicio 3: E-Commerce
+    //Crea una clase padre Producto con protected float costoFabricacion y private float
+    //precioVenta.Crea una clase hija llamada ProductoImportado.
+    //Regla de negocio: La clase hija debe tener un método que calcule los impuestos
+    //de importación basándose en el costoFabricacion (al ser protected, el hijo puede
+    //leerlo). Sin embargo, intentar modificar el precioVenta desde la clase hija debe dar
+    //error, obligando a usar un setter público definido en el padre
+
+    public class ProductoImportado : Producto 
+    {
+        public float IVA = 0.21f;
+
+        public ProductoImportado(float costoFabricacion) : base(costoFabricacion)
+        {
+        }
+
+        public float CalcularImpuestos()
+        {
+            return this.costoFabricacion * IVA;
+        }
+    }
+    public class Producto
+    {
+        protected float costoFabricacion { get; set; }
+        private float precioVenta { get; set; }
+
+        public Producto(float costoFabricacion)
+        {
+            this.costoFabricacion = costoFabricacion;
+        }
+
+        public void setPrecioVenta(float precioVenta)
+        {
+            this.precioVenta = precioVenta;
+        }
+    }
+
+
+    //Ejercicio 4: Sistema de Usuarios
+    //Definí una clase Usuario con private string password.
+    //Regla de negocio: El setter de la contraseña debe verificar que el value recibido
+    //tenga al menos 8 caracteres.Si tiene menos, debe rechazar la asignación y avisar
+    //por consola. Desde el Main, instancia un usuario e intenta ponerle la contraseña
+    //"123"
+
+    public class Usuario
+    {
+        private string username;
+        private string password;
+
+        public Usuario(string username, string password)
+        {
+            this.username = username;
+            setPassword(password);
+        }
+
+        public void setPassword(string password)
+        {
+            if(password.Length < 8)
+            {
+                Console.WriteLine("La contraseña debe tener 8 caracteres o más. Intentelo nuevamente.");
+                return;
+            }
+            this.password = password;
+        }
+    }
+
+    //Ejercicio 5: La Red Corporativa
+    //Crea una clase Servidor con protected string direccionIP y private bool
+    //estaConectado.
+    //Regla de negocio: La variable estaConectado solo debe tener un Getter público
+    //(sin setter público). Para que el servidor se conecte, debes crear un método
+    //público llamado HacerPing(), que internamente cambie estaConectado a true.
+    //Esto demuestra cómo proteger un estado de modificaciones directas desde el
+    //Main
+
+    public class Servidor
+    {
+        protected string direccionIP;
+        private bool estaConectado;
+
+        public Servidor(string direccion)
+        {
+            this.direccionIP = direccion;
+            this.estaConectado = false;
+        }
+
+        public bool getEstaConectado()
+        {
+            return this.estaConectado;
+        }
+
+
+        public void HacerPing()
+        {
+            this.estaConectado = true;
+        }
+    }
+
+    //Ejercicio 6: El Zoológico Virtual
+    //Crea una clase abstracta Animal con una propiedad Nombre y un método
+    //abstracto HacerRuido().Crea tres clases hijas: Perro, Gato y Vaca, que hereden de
+    //Animal y sobreescriban al método HacerRuido().
+    //Implementación: En el Main, crea un arreglo de tipo Animal[] con 3 posiciones.
+    //Guarda un Perro, un Gato y una Vaca en cada posición.Usa un bucle foreach para
+    //recorrer el arreglo y llama al método HacerRuido() de cada uno.
+
+    public class Vaca : Animal
+    {
+        public Vaca(string nombre) : base(nombre)
+        {
+        }
+
+        public override void HacerRuido()
+        {
+            Console.WriteLine("La vaca está mugiendo...");
+        }
+    }
+
+    public class Gato : Animal
+    {
+        public Gato(string nombre) : base(nombre)
+        {
+        }
+
+        public override void HacerRuido()
+        {
+            Console.WriteLine("El gato está maullando...");
+        }
+    }
+
+    public class Perro : Animal
+    {
+        public Perro(string nombre) : base(nombre)
+        {
+        }
+
+        public override void HacerRuido()
+        {
+            Console.WriteLine("El perro está ladrando...");
+        }
+    }
+
+    public abstract class Animal
+    {
+        private string Nombre;
+
+        public Animal(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+        public abstract void HacerRuido();
+    }
+
+    //Ejercicio 7: Monitoreo de Infraestructura IT
+    //Crea una clase abstracta DispositivoRed con un atributo DireccionIP y un método
+    //abstracto EjecutarDiagnostico(). Crea dos clases hijas: Router(su diagnóstico
+    //imprime por consola "Revisando tablas de ruteo en {IP}") y Switch(su diagnóstico
+    //imprime por consla "Revisando puertos VLAN en {IP}").
+    //Implementación: Instancia un arreglo DispositivoRed[] datacenter = new
+    //DispositivoRed[4]; mezclando Routers y Switches.Recorre el arreglo con un for o
+    //foreach y ejecuta el diagnóstico de todos los equipos con una sola línea de código
+    //dentro del bucle.
+
+    public class Switch : DispositivoRed
+    {
+        public Switch(string direccionIP) : base(direccionIP)
+        {
+        }
+
+        public override void EjecutarDiagnostico()
+        {
+            Console.WriteLine($"Revisando puertos VLAN en {this.DireccionIP}");
+        }
+    }
+
+    public class Router : DispositivoRed
+    {
+        public Router(string direccionIP) : base(direccionIP)
+        {
+        }
+
+        public override void EjecutarDiagnostico()
+        {
+            Console.WriteLine($"Revisando tablas de ruteo en {this.DireccionIP}");
+        }
+    }
+
+    public abstract class DispositivoRed
+    {
+        public string DireccionIP;
+
+        public DispositivoRed(string direccionIP)
+        {
+            this.DireccionIP = direccionIP;
+        }
+
+        public abstract void EjecutarDiagnostico();
+    }
+}
---
+++ ejerciciosListas2/ejerciciosListas2/Program.cs
@@ -0,0 +1,227 @@
+﻿using System.Net;
+using System.Numerics;
+using System.Security.Claims;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ejerciciosListas2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // EJERCICIO 1
+            Colectivo colectivo1 = new Colectivo("98", 85, 25);
+            Colectivo colectivo2 = new Colectivo("65", 85, 10);
+            Colectivo colectivo3 = new Colectivo("151", 92, 32);
+            Colectivo colectivo4 = new Colectivo("603", 85, 1);
+
+            Colectivo[] colectivos = { colectivo1, colectivo2, colectivo3, colectivo4 };
+
+            calcularMetricas(colectivos);
+
+            // EJERCICIO 2
+
+            Jugador jugador1 = new Jugador("Driussi", 9, 12);
+            Jugador jugador2 = new Jugador("Juanfer", 10, 8);
+            Jugador jugador3 = new Jugador("Salas", 7, 0);
+            Jugador jugador4 = new Jugador("Montiel", 29, 7);
+            Jugador jugador5 = new Jugador("Acuña", 34, 3);
+            Jugador[] jugadores = { jugador1, jugador2, jugador3, jugador4, jugador5 };
+
+            analizarEquipo(jugadores);
+
+            // EJERCICIO 3
+
+            Libro libro1 = new Libro("El Aleph", "Borges", 1945);
+            Libro libro2 = new Libro("Telón", "Agatha Christie", 1975);
+            Libro libro3 = new Libro("Ficciones", "Borges", 1944);
+            Libro libro4 = new Libro("Los siete locos", "Roberto Arlt", 1929);
+            Libro libro5 = new Libro("Los lanzallamas", "Roberto Arlt", 1931);
+
+            Libro[] libros = { libro1, libro2, libro3, libro4, libro5 };
+
+            string autorBuscado = "Borges";
+            BuscarLibro(autorBuscado, libros);
+
+            // EJERCICIO 4 
+            Flor flor1 = new Flor("Rosa");
+            Flor flor2 = new Flor("Lavanda");
+
+            Arbol arbol1 = new Arbol("Limonero");
+            Arbol arbol2 = new Arbol("Jacaranda");
+
+            Planta[] plantas = { flor1, flor2, arbol1, arbol2 };
+
+
+            ListarPlantas(plantas);
+            
+        }
+        //Control de Flota de Colectivos
+        //Una empresa de transporte urbano necesita un reporte del estado de sus unidades en circulación.
+        //Defina la clase Colectivo con los atributos Linea (entero o string), CapacidadMaxima(entero) y PasajerosActuales(entero).
+        //En el programa principal, instancie un arreglo de 4 objetos Colectivo con datos ficticios.
+        //Recorra el arreglo para determinar e informar por pantalla dos métricas:
+        //La cantidad total de pasajeros que está transportando la flota en este momento(sumatoria).
+        //La línea del colectivo que tiene la mayor cantidad de asientos libres(calculado como CapacidadMaxima - PasajerosActuales).
+
+        public class Colectivo
+        {
+            public string Linea { get; set; }
+            public int CapacidadMaxima { get; set; }
+            public int PasajerosActuales { get; set; }
+
+            public Colectivo(string linea, int capacidadMaxima, int pasajerosActuales)
+            {
+                this.Linea = linea;
+                this.CapacidadMaxima = capacidadMaxima;
+                this.PasajerosActuales = pasajerosActuales;
+            }
+        }
+
+
+        public static void calcularMetricas(Colectivo[] colectivos)
+        {
+            int cantidadTotalPasajeros = 0;
+
+            int diferenciaAsientos = 0;
+            string lineaConMayorPasajeros = "";
+            foreach (Colectivo colectivo in colectivos)
+            {
+                cantidadTotalPasajeros += colectivo.PasajerosActuales;
+
+                diferenciaAsientos = colectivo.CapacidadMaxima - colectivo.PasajerosActuales;
+                for (int i = 1; i < colectivos.Length - 1; i++)
+                {
+                    int diferenciaActual = colectivos[i].CapacidadMaxima - colectivos[i].PasajerosActuales;
+                    if (diferenciaAsientos > diferenciaActual)
+                    {
+                        lineaConMayorPasajeros = colectivo.Linea;
+                    }
+                    else
+                    {
+                        lineaConMayorPasajeros = colectivos[i].Linea;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Los colectivos tienen un total de {cantidadTotalPasajeros} pasajeros. \nLa linea con mas asientos libres es {lineaConMayorPasajeros}.");
+        }
+
+
+        //Estadísticas del Plantel de Fútbol
+        //El cuerpo técnico necesita un análisis rápido del rendimiento goleador del equipo.
+        //Cree la clase Jugador con los atributos Nombre(string), NroCamiseta(entero) y GolesMarcados(entero). 
+        //Declare e instancie un arreglo con 5 jugadores, asignando distintas cantidades de goles a cada uno.
+        //Mediante un ciclo de repetición, analice el arreglo para identificar al goleador del equipo.
+        //El sistema debe imprimir el nombre del jugador con más goles.Además, debe calcular y mostrar el promedio de goles del plantel completo.
+
+        public class Jugador
+        {
+            public string Nombre { get; set; }
+            public int NroCamiseta { get; set; }
+            public int GolesMarcados { get; set; }
+
+            public Jugador(string nombre, int nroCamiseta, int golesMarcados)
+            {
+                Nombre = nombre;
+                NroCamiseta = nroCamiseta;
+                GolesMarcados = golesMarcados;
+            }
+        }
+
+        public static void analizarEquipo(Jugador[] jugadores)
+        {
+            int golesTotales = 0;
+            int masGoles = jugadores[0].GolesMarcados;
+
+            string goleador = jugadores[0].Nombre;
+            for (int i = 0; i < jugadores.Length; i++)
+            {
+                golesTotales += jugadores[i].GolesMarcados;
+
+                if (jugadores[i].GolesMarcados > masGoles)
+                {
+                    masGoles = jugadores[i].GolesMarcados;
+                    goleador = jugadores[i].Nombre;
+                }
+            }
+
+            double promedioGol = golesTotales / jugadores.Length;
+
+            Console.WriteLine($"El goleador es {goleador}. El promedio de gol en el equipo es de {promedioGol}");
+        }
+
+
+        //Buscador de la Biblioteca Barrial
+        //Una biblioteca necesita filtrar su catálogo de libros rápidamente sin depender de bases de datos complejas.
+        //Defina la clase Libro con los atributos Titulo(string), Autor(string) y AnioPublicacion(entero).
+        //Cree un arreglo de 4 libros.Asegurate de que al menos dos libros pertenezcan al mismo autor(por ejemplo, "Borges").
+        //Declare una variable independiente string autorABuscar = "Borges"(o el autor que prefieras).
+        //Recorre el arreglo de objetos y muestre por pantalla únicamente los títulos de los libros cuyo atributo Autor coincida exactamente con la variable buscada.
+
+        public class Libro
+        {
+            public string Titulo { get; set; }
+            public string Autor { get; set; }
+            public int AnioPublicacion { get; set; }
+
+            public Libro(string titulo, string autor, int anioPublicacion)
+            {
+                Titulo = titulo;
+                Autor = autor;
+                AnioPublicacion = anioPublicacion;
+            }
+        }
+
+        public static void BuscarLibro(string autor, Libro[] libros)
+        {
+            for (int i = 0; i < libros.Length; i++)
+            {
+                if (autor.Equals(libros[i].Autor))
+                {
+                    Console.WriteLine($"Autor encontrado. Libro: {libros[i].Titulo}");
+                }
+            }
+        }
+
+        //Relevamiento de Espacios Verdes(Introducción al Polimorfismo)
+        //El área de espacio público necesita listar rápidamente las distintas especies plantadas en una plaza, sin importar su tipo específico.
+        //Clase Padre: Cree una clase Planta que contenga únicamente el atributo NombreEspecie(string) y su respectivo constructor.
+        //Clases hijas: Cree dos clases que hereden de Planta: Arbol y Flor.
+        //Ambas deben tener un constructor que reciba el nombre y se lo pase al constructor de la clase padre.
+        //No es necesario agregarles atributos extra a las clases hijas.
+        //Estructura Polimórfica: En el programa principal, instancie un arreglo de tipo Planta con capacidad para 4 elementos.
+        //Asigne en las distintas posiciones del arreglo una mezcla de clases hijas
+        //(por ejemplo, dos objetos de tipo Arbol como "Jacarandá" y "Palo Borracho", y dos de tipo Flor como "Jazmín" y "Margarita").
+        //Utilice un ciclo iterativo para recorrer el arreglo base e imprima por pantalla una lista con el NombreEspecie de cada elemento.
+        public class Planta
+        {
+            public string NombreEspecie {  get; set; }
+
+            public Planta(string nombreEspecie)
+            {
+                NombreEspecie = nombreEspecie;
+            }
+        }
+
+        public class Arbol : Planta
+        {
+            public Arbol(string nombreEspecie) : base(nombreEspecie) { }
+        }
+
+        public class Flor : Planta
+        {
+            public Flor(string nombreEspecie) : base(nombreEspecie) { }
+        }
+
+        public static void ListarPlantas(Planta[] plantas)
+        {
+            foreach (Planta planta in plantas)
+            {
+                Console.WriteLine($"Planta del tipo {planta.NombreEspecie}");
+            }
+        }
+    }
+}
